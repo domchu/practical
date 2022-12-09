fc23291b9993bb7430ef6ed9388004ad7f65fd67
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-12-10 00:32:30 +0100

    chore:first commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,18 @@ import { links, social } from "./data";
 import logo from "./logo.svg";
 
 const Navbar = () => {
-  return <h4>navbar</h4>;
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="link-container show-container">
+          <ul className="social-icons"></ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
