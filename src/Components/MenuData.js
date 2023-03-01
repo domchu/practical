626d705d1b5a5071159b026298d915497f21c69d
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-03-01 12:49:42 +0100

    chores: added all dependencies & created pages

---
+++ src/Components/MenuData.js
@@ -0,0 +1,26 @@
+import { FaHome } from "react-icon/fa";
+import { BsBlockquoteLeft } from "react-icon/bsb";
+import { FcBusinessContact } from "react-icon/fcb";
+import { FcClock } from "react-icon/fc";
+const data = [
+  {
+    title: "Home",
+    path: "/",
+    icons: <FaHome />,
+  },
+  {
+    title: "Home",
+    path: "/",
+    icons: <FcBusinessContact />,
+  },
+  {
+    title: "Home",
+    path: "/",
+    icons: <BsBlockquoteLeft />,
+  },
+  {
+    title: "Home",
+    path: "/",
+    icons: <FcClock />,
+  },
+];
