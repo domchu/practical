a10223581d049a2402c4720ff8ea9ea9d5f1e3c9
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-03-10 01:50:07 +0100

    chores:separate all the items in each pages

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Box } from "@chakra-ui/react";
+import Counter from "../Counter/Counter";
 
 const Blog = () => {
   return (
     <Box style={{ fontSize: "3rem" }} className="page">
       <h2>Blog page</h2>
+      <Counter />
     </Box>
   );
 };
--- src/Pages/Faq.js
+++ src/Pages/Faq.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Box } from "@chakra-ui/react";
+import Props from "../Props/Props";
 
 const Faq = () => {
   return (
     <Box style={{ fontSize: "3rem" }} className="page">
       <h2>FAQ page</h2>
+      <Props />
     </Box>
   );
 };
