190d0b51cdf89aa0c58e1407309e69b4157e9ea4
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-03-17 22:13:09 +0100

    chores:change the about btns colors

--- src/Pages/about/About.js
+++ src/Pages/about/About.js
@@ -5,12 +5,22 @@ import { Box } from "@chakra-ui/react";
 const About = () => {
   return (
     <Box style={{ fontSize: "1rem" }}>
-      <Box textAlign="center" className="about-btn">
-        <Link to="/about/search">Search</Link>
-        <NavLink to="/about/list" activeClassName="active">
+      <Box textAlign="center" className="about-bt1n ">
+        <Link to="/about/search" className="btn btn-primary">
+          Search
+        </Link>
+        <NavLink
+          to="/about/list"
+          activeClassName="active"
+          className="btn btn-secondary my-5"
+        >
           List
         </NavLink>
-        <NavLink to="/about/add" activeClassName="active">
+        <NavLink
+          to="/about/add"
+          activeClassName="active"
+          className="btn btn-success my-5"
+        >
           Add
         </NavLink>
       </Box>
